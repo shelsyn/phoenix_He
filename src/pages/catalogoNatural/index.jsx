e3ebbdc8e8a2cbb3e64a4eaf86b5c8ff87a51e47
camilo <camiloquinterovargas132@gmail.com>
2023-04-02 17:31:56 -0500

    [FIX] view airpad

--- src/pages/catalogoNatural/index.jsx
+++ src/pages/catalogoNatural/index.jsx
@@ -258,7 +258,7 @@ export default function CatalogoPersonaNatural() {
       <RentingNatural />
       <section id="catalogoEmpresa">
         <div className="bg-white">
-          <div className="mx-auto max-w-2xl py-16 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="mx-auto py-16 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
             <h2 className="text-4xl text-gold-goldTitle font-bold ">Nuestro Catálogo</h2>
             <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
               {products.map((product) => (
